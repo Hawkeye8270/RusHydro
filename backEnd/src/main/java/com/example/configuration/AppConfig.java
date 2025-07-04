77460843aa456d39d575576a1df19bed212c0430
User <1@1>
2025-07-04 22:20:58 +0300

    Dockerfile backend загружается. Front почищен. backEnd почищен. AutoCrawling работает автономно.

--- backEnd/src/main/java/com/example/configuration/AppConfig.java
+++ backEnd/src/main/java/com/example/configuration/AppConfig.java
@@ -1,7 +1,10 @@
 package com.example.configuration;
 
+import com.p6spy.engine.spy.P6DataSource;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,12 +13,14 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import javax.sql.DataSource;
 import java.util.Properties;
 
+
 @Configuration
 @EnableTransactionManagement
 public class AppConfig {
 
+
     @Bean
-    public MappingJackson2JsonView jsonMapper(){
+    public MappingJackson2JsonView jsonMapper() {
         MappingJackson2JsonView view = new MappingJackson2JsonView();
         view.setPrettyPrint(true);
         return view;
@@ -36,16 +41,40 @@ public class AppConfig {
         return properties;
     }
 
+//    //  РАБОТАЕТ БЕЗ DOCKER
     @Bean
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
-        dataSource.setJdbcUrl("jdbc:p6spy:postgresql://localhost:5432/rushydro");
+//        dataSource.setJdbcUrl("jdbc:p6spy:postgresql://localhost:5432/rushydro");
+        dataSource.setJdbcUrl("jdbc:p6spy:postgresql://db:5432/rushydro");
+//        dataSource.setJdbcUrl("jdbc:p6spy:postgresql://my-postgres:5432/rushydro");
         dataSource.setUsername("postgres");
         dataSource.setPassword("123");
         dataSource.setMaximumPoolSize(10);
         return dataSource;
     }
+
+
+//
+//    private final org.springframework.core.env.Environment environment;
+//    public AppConfig(org.springframework.core.env.Environment environment) {
+//        this.environment = environment;
+//    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        // 1. Сначала создаем обычный DataSource
+//        HikariDataSource actualDataSource = new HikariDataSource();
+//        actualDataSource.setJdbcUrl("jdbc:postgresql://db:5432/rushydro");
+//        actualDataSource.setDriverClassName("org.postgresql.Driver");
+//        actualDataSource.setUsername("postgres");
+//        actualDataSource.setPassword("123");
+//        actualDataSource.setMaximumPoolSize(10);
+//
+//        // 2. Оборачиваем в P6Spy
+//        return new P6DataSource(actualDataSource);
+//    }
 
 
 }
